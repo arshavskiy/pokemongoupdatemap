ac2e2f1c33139e312e529159830e1482f5fc5f8c
arshavskiy <pasha.websbee@gmail.com>
2017-05-11 00:35:42 +0300

    old files cleanup

--- test/func.js
+++
@@ -1,7 +0,0 @@
- $(document).ready(function () {
-     if (beaches) {
-         for (i = 0; i < beaches.length; i++) {
-             $('<h3>').html(beaches[i][0]).appendTo('#container');
-         }
-     }
- });
