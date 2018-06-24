779def699252726b52d5c9b3c9412f3ed4151903
Pavel <arshavsky.pasha@gmail.com>
2018-06-24 15:39:42 +0300

    function removed, style added

--- js/tochMap.js
+++ js/tochMap.js
@@ -43,44 +43,61 @@ function addMarker(location, map, label, icon) {
     
 
     google.maps.event.addListener(marker, 'click', function(e) {
-        let labelM = this.labelContent;
-        let iconM = this.icon;
-        let subMenu;
+        // let labelM = this.labelContent;
+        // let iconM = this.icon;
+        // let subMenu;
 
-        $('#pokistopModal').on('show.bs.modal', function (e) {
-            // if (!data) return e.preventDefault();
-            // window.location.href = this.url;
-                $('#header').html(labelM);
-                $('#icon').html(`<img src="${iconM}"></img>`);
+        let menu = $('.icon-first_menu>div');
+        let menuSecond = $('icon-second_menu>div');
+        let backBtn = $('#back-second_menu');
+        let exitBtn = $('#exit-first_menu');
+        let earnExitBtn = $('#back-earn_menu');
+        let firstMenuModal = $('#pokistopModal');
 
-                let menu = $('.icon-first_menu img');
-                menu.each(function(i ,e){
-                    e.addEventListener('click', function (e) {
-                        console.log('e', e.target);
-                        if(e.target.id == 1){
-                            
-                            $('#pokistopModal').modal('hide');
+        exitBtn.click( function (e) {
+            $('#pokistopModal').modal('hide');
+        });
 
-                            $('#pokiModalSubMenu').modal('show');
-                            $('#pokiModalSubMenu').on('show.bs.modal', function (e) {
-                                subMenu = `<img id="${i}" src="https://vignette.wikia.nocookie.net/pokemongo/images/6/65/Stardust.png" >`;
-                                $('.icon-sub_menu').html(`${subMenu}`);
+        backBtn.click( function (e) {
+            $('#pokistopModalSubMenu').modal('hide');
+            firstMenuModal.modal('show');
+        });
+        earnExitBtn.click( function (e) {
+            $('#pokistopModalSubEarn').modal('hide');
+            firstMenuModal.modal('show');
+        });
+        
+
+        // $('#pokistopModal').on('show.bs.modal', function (e) { });
+        menu.each(function(i ,e){
+            e.addEventListener('click', function (e) {
+                    switch (e.path[0].id) {
+                        case 'a7':
+                            $('#pokistopModalSubEarn').modal('show');
+                            firstMenuModal.modal('hide');
+                            break;
+
+                        default:
+                            $('#pokistopModalSubMenu').modal('show');
+                            firstMenuModal.modal('hide');
+
+                            $('#pokistopModalSubMenu').on('show.bs.modal', function (e) {
                             });
-                        }
-                    });       
-                }) 
+                    }
+            });       
+        });
                     
 
-          });
+         
         $('#pokistopModal').modal('show');
           
     });
 
-    if (typeof location == 'object'){
-        printCordinates(location.lat, location.lng, label, map, 'https://raw.githubusercontent.com/arshavskiy/google_maps_api_page/testing/icons/003-insignia.png');
-    } else {
-        printCordinates(location.lat(), location.lng(), label, map);
-    }
+    // if (typeof location == 'object'){
+    //     printCordinates(location.lat, location.lng, label, map, 'https://raw.githubusercontent.com/arshavskiy/google_maps_api_page/testing/icons/003-insignia.png');
+    // } else {
+    //     printCordinates(location.lat(), location.lng(), label, map);
+    // }
 }
 
 
